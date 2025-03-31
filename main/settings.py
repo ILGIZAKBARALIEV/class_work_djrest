05db20c485e802f084ef4609bb16e5a53f0255a8
ILGIZ_AKBARALIEV <okoo.goe@gmail.com>
2025-04-01 01:11:09 +1100

    lesson_8

--- main/settings.py
+++ main/settings.py
@@ -48,7 +48,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
-    'PAGE_SIZE': 3
+    'PAGE_SIZE': 3,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
 
 MIDDLEWARE = [
